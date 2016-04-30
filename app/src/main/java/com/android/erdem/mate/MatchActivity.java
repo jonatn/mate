bd39c3a5106824cafa7c289376447f049ce6f94f
ebozkurt93 <ebozkurt93@gmail.com>
2016-04-30 16:44:19 +0300

    //ui updates

--- app/src/main/java/com/android/erdem/mate/MatchActivity.java
+++ app/src/main/java/com/android/erdem/mate/MatchActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 public class MatchActivity extends AppCompatActivity {
 
     private TextView peopleamount, match;
-    private ImageView image1, image2, image3, image4;
+    private ImageView image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +26,19 @@ public class MatchActivity extends AppCompatActivity {
         actionBar.setIcon(R.drawable.mate_logo_white);
 
         peopleamount = (TextView)findViewById(R.id.matchactivity_people_amount_text);
-        match = (TextView)findViewById(R.id.matchactivity_match);
-        image1 = (ImageView)findViewById(R.id.matchactivity_profile_image1);
-        image2 = (ImageView)findViewById(R.id.matchactivity_profile_image2);
-        image3 = (ImageView)findViewById(R.id.matchactivity_profile_image3);
-        image4 = (ImageView)findViewById(R.id.matchactivity_profile_image4);
+        match = (TextView)findViewById(R.id.matchactivity_match_text);
+        image1 = (ImageView)findViewById(R.id.matchactivity_avatar_image1);
+        image2 = (ImageView)findViewById(R.id.matchactivity_avatar_image2);
+        image3 = (ImageView)findViewById(R.id.matchactivity_avatar_image3);
+        image4 = (ImageView)findViewById(R.id.matchactivity_avatar_image4);
+        image5 = (ImageView)findViewById(R.id.matchactivity_avatar_image5);
+        image6 = (ImageView)findViewById(R.id.matchactivity_avatar_image6);
+        image7 = (ImageView)findViewById(R.id.matchactivity_avatar_image7);
+        image8 = (ImageView)findViewById(R.id.matchactivity_avatar_image8);
+        image9 = (ImageView)findViewById(R.id.matchactivity_avatar_image9);
+        image10 = (ImageView)findViewById(R.id.matchactivity_avatar_image10);
+        image11 = (ImageView)findViewById(R.id.matchactivity_avatar_image11);
+        image12 = (ImageView)findViewById(R.id.matchactivity_avatar_image12);
 
         //need to add images from server also instead of empty images to here
 
