75ec1fde2c58db23f011f4139dd63ffa032f73c5
ebozkurt93 <ebozkurt93@gmail.com>
2016-05-03 13:06:27 +0300

    //Redesign of cardgame interface, switched to recyclerview

---
+++ app/src/main/java/com/android/erdem/mate/OnItemClickListener.java
@@ -0,0 +1,26 @@
+package com.android.erdem.mate;
+
+/**
+ * Created by erdem on 2.05.2016.
+ */
+import android.view.View;
+
+/**
+ * Interface definition for a callback to be invoked when an item in this collection view has been
+ * clicked.
+ */
+public interface OnItemClickListener {
+
+    /**
+     * Callback method to be invoked when an item in this AdapterView has
+     * been clicked.
+     * <p>
+     * Implementers can call getChildAt(position) if they need to access the data associated with
+     * the selected item.
+     *
+     * @param view The view within the AdapterView that was clicked (this will be a view provided
+     *             by the adapter)
+     * @param position The position of the view in the adapter.
+     */
+    public void onItemClick(View view, int position);
+}
---
+++ app/src/main/java/com/android/erdem/mate/adapter/ViewAdapter.java
@@ -0,0 +1,25 @@
+package com.android.erdem.mate.adapter;
+
+/**
+ * Created by erdem on 2.05.2016.
+ */
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AbsListView;
+
+/**
+ * Interface for a given UI element to help extend the swipe-to-dismiss-undo pattern to other
+ * elements.
+ */
+public interface ViewAdapter {
+    Context getContext();
+    int getWidth();
+    int getChildCount();
+    void getLocationOnScreen(int[] locations);
+    View getChildAt(int index);
+    int getChildPosition(View position);
+    void requestDisallowInterceptTouchEvent(boolean disallowIntercept);
+    void onTouchEvent(MotionEvent e);
+    Object makeScrollListener(AbsListView.OnScrollListener listener);
+}
