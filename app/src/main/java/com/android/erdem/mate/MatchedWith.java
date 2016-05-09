ec3eea9e4158f1690dca26b6adcb92c76c5514e9
ebozkurt93 <ebozkurt93@gmail.com>
2016-05-09 16:58:31 +0300

    //small ui adjustments 2
    Signed-off-by: ebozkurt93 <ebozkurt93@gmail.com>

---
+++ app/src/main/java/com/android/erdem/mate/MatchedWith.java
@@ -0,0 +1,20 @@
+package com.android.erdem.mate;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+public class MatchedWith extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matched_with);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(null);
+        actionBar.setSubtitle(null);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.mate_logo_white);
+    }
+}
